2e28bd5020c3d4c432a365085e816d6860caf1c8
smurfjames101 <smurfjames@gmail.com>
2019-04-02 11:37:32 +0100

    Renamed Constants File To Match Convention

--- src/main/java/com/qa/landlordGateway/MainConstants.java
+++
@@ -1,9 +0,0 @@
-package com.qa.landlordGateway;
-
-public class MainConstants {
-public static final String CREATE_LANDLORD_PATH = "/createLandlord";
-public static final String GET_ALL_LANDLORDS_PATH = "/getAllLandlords";
-public static final String LANDLORD_SERCH_PATH = "/lanlordSearch";
-
-
-}
