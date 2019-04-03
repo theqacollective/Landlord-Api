1e7865ac8d33c7a43e305034ce51e2749ca58eba
AntonyTruran <tonytruran@gmail.com>
2019-04-03 11:07:31 +0100

    landlord search path corrected from lanlord

--- src/main/java/com/qa/landlordGateway/controllers/LandlordController.java
+++ src/main/java/com/qa/landlordGateway/controllers/LandlordController.java
@@ -24,42 +24,48 @@ public class LandlordController {
 
 	private LandlordService landlordService;
 	private LandlordBuilder landlordBuilder;
-	
+
 	public LandlordController(LandlordService landlordService, LandlordBuilder landlordBuilder) {
 		this.landlordService = landlordService;
 		this.landlordBuilder = landlordBuilder;
 	}
-	
+
 	@PostMapping(Constants.CREATE_LANDLORD_PATH)
 	public ResponseEntity<Landlord> createLandlord(@RequestBody Landlord landlord) {
 		return this.landlordService.createLandlord(landlord);
 	}
+
 	@GetMapping(Constants.GET_ALL_LANDLORDS_PATH)
-	public List<Landlord> getLandlords(){
+	public List<Landlord> getLandlords() {
 		return this.landlordService.getLandlords();
 	}
+
 	@GetMapping(Constants.LANDLORD_SERCH_PATH)
-	public List<Landlord> landlordSearch(String firstName, String lastName, String email, String phoneNumber){
-		return this.landlordService.landlordSearch(landlordBuilder.firstName(firstName).lastName(lastName).email(email).phoneNumber(phoneNumber).landlordBuild());
+	public List<Landlord> landlordSearch(String firstName, String lastName, String email, String phoneNumber) {
+		return this.landlordService.landlordSearch(landlordBuilder.firstName(firstName).lastName(lastName).email(email)
+				.phoneNumber(phoneNumber).landlordBuild());
 	}
+
 	@DeleteMapping(Constants.DELETE_URL)
-	public String deleteLandlord(String firstName, String lastName,String email, String phoneNumber) {
-		
-		List<Landlord> landlords = this.landlordSearch(firstName,lastName,email,phoneNumber);
+	public String deleteLandlord(String firstName, String lastName, String email, String phoneNumber) {
+
+		List<Landlord> landlords = this.landlordSearch(firstName, lastName, email, phoneNumber);
 
 		for (int i = 0; i < landlords.size(); i++) {
 			this.landlordService.deleteLandlord(landlords.get(i));
 		}
 		return Constants.getDeletionMessage();
 	}
+
 	private RestTemplateBuilder rtb;
 
 	@PutMapping(Constants.UPDATE_URL)
-	public String updateLandlord(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName, @RequestBody Landlord updateLandlord)
-	{
+	public String updateLandlord(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName,
+			@RequestBody Landlord updateLandlord) {
 		this.landlordService.updateLandlord(firstName, lastName, updateLandlord);
 		return Constants.getUpdateMesssage();
 	}
+
 	public RestTemplateBuilder getRtb() {
 		return rtb;
 	}
@@ -67,6 +73,5 @@ public class LandlordController {
 	public void setRtb(RestTemplateBuilder rtb) {
 		this.rtb = rtb;
 	}
-	
-}
 
+}
