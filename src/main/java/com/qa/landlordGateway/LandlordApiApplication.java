31401faf036601a99690ec4dbde285e5c87ae342
AntonyTruran <tonytruran@gmail.com>
2019-04-04 11:01:22 +0100

    eureka changes

--- src/main/java/com/qa/landlordGateway/LandlordApiApplication.java
+++ src/main/java/com/qa/landlordGateway/LandlordApiApplication.java
@@ -2,8 +2,12 @@ package com.qa.landlordGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableDiscoveryClient
 public class LandlordApiApplication {
 
 	public static void main(String[] args) {
